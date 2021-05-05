bd5c9ea6f4963134ef58f2797cbb3876e5e51211
Juli Ivance <ulya-rubo@mail.ru>
2021-05-05 21:32:09 +0300

    commit3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-Person men1 = new Person("Nell", "Teo", new Address("Vitebsk", "Pobeda", 5));
-        Person men1 = new Person("Nell", "Teo", new Address("Vitebsk", "Pobeda", 5));
-        Person men1 = new Person("Nell", "Teo", new Address("Vitebsk", "Pobeda", 5));
 
+        Person m1 = new Person("Nell", "Teo", new Address("Vitebsk", "Pobeda", 5));
+        Person m2 = new Person("Well", "Tig", new Address("Vitebsk", "lazo", 7));
+        Person m3 = new Person("Nell", "Teo", new Address("Vitebsk", "Pobeda", 5));
 	// write your code here
     }
 }

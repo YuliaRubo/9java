6be801757d9e0811e9b94eb24b104561e4a70013
Ulya Ivancova <ulya-rubo@mail.ru>
2021-05-26 14:25:09 +0200

    commit1

--- src/com/company/PersonIOUtil.java
+++ src/com/company/PersonIOUtil.java
@@ -1,13 +1,29 @@
 package com.company;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class PersonIOUtil {
 
 
-        public static void writePersons(String fileName, Person persons) {
-            //Определяем файл
-            File file = new File("fileName.txt");
+        public static void writePersons(String fileName, Person persons) throws IOException {
+            try(FileWriter writer = new FileWriter("notes3.txt", false))
+            {
+                // запись всей строки
+                String text = "Hello Gold!";
+                writer.write(text);
+                // запись по символам
+                writer.append('\n');
+                writer.append('E');
+
+                writer.flush();
+            }
+            catch(IOException ex){
+
+                System.out.println(ex.getMessage());
+            }
         }
-    }
 }
+
+
